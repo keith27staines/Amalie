4d2ead650ecee304fead77c6a758d637ed138a48
Keith Staines <keith27staines@me.com>
2013-08-05 07:37:09 +0100

    Repaired misnamed VariableContentView and assigned correct class to custom view in Graph2D

---
+++ Amalie/Amalie/AMVariableContentView.h
@@ -0,0 +1,13 @@
+//
+//  AMVariableContentView.h
+//  Amalie
+//
+//  Created by Keith Staines on 03/08/2013.
+//  Copyright (c) 2013 Keith Staines. All rights reserved.
+//
+
+#import "AMContentView.h"
+
+@interface AMVariableContentView : AMContentView
+
+@end
---
+++ Amalie/Amalie/AMVariableContentView.m
@@ -0,0 +1,29 @@
+//
+//  AMVariableContentView.m
+//  Amalie
+//
+//  Created by Keith Staines on 03/08/2013.
+//  Copyright (c) 2013 Keith Staines. All rights reserved.
+//
+
+#import "AMVariableContentView.h"
+
+@implementation AMVariableContentView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    // Drawing code here.
+}
+
+@end
